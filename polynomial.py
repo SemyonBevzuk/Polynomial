3be3a1388a40ba903fbbae96c33752a1f3d906ca
Semen Bevzuk <semen.bevzuk@gmail.com>
2019-04-06 22:17:04 +0300

    Add Python lab

---
+++ polynomial.py
@@ -0,0 +1,232 @@
+class Polynomial:
+    def get_coefficients(self):
+        return self.__coefficients
+
+    def degree(self):
+        return len(self.__coefficients) - 1
+
+    def __init__(self, *args):
+        if not args:
+            self.__coefficients = [0]
+        elif isinstance(args[0], (tuple, list, Polynomial)):
+            if isinstance(args[0], Polynomial):
+                self.__coefficients = [a for a in args[0].get_coefficients()]
+            elif args[0] or len(args[0]) != 0:
+                if self.__is_list_correct(list(args[0])):
+                    self.__coefficients = self.__remove_zeros_list(list(args[0]))
+                else:
+                    raise ValueError('Invalid argument in init. The list/tuple must store int.')
+            else:
+                self.__coefficients = [0]
+        else:
+            raise TypeError('Invalid argument in init. Need nothing, Polynomial, list or tuple.')
+
+    def __is_list_correct(self, coefficients):
+        result = True
+        for a in coefficients:
+            if not isinstance(a, int):
+                result = False
+                break
+        return result
+
+    def __remove_zeros_list(self, coefficients):
+        short_coefficients = []
+        flag_no_first_nonzero = True
+        for a in coefficients:
+            if a == 0 and flag_no_first_nonzero:
+                continue
+            else:
+                flag_no_first_nonzero = False
+                short_coefficients.append(a)
+        if flag_no_first_nonzero:
+            short_coefficients.append(0)
+        return short_coefficients
+
+    def __remove_zeros_polynomial(self):
+        self.__coefficients = self.__remove_zeros_list(self.__coefficients)
+
+    def __str__(self):
+        string = ''
+        i = 0
+        current_degree = self.degree()
+        if current_degree == 0:
+            if self.__coefficients[i] < 0:
+                string += '- '
+            string += str(abs(self.__coefficients[i]))
+        else:
+            if self.__coefficients[i] < 0:
+                string += '- '
+            if abs(self.__coefficients[i]) != 1:
+                string += str(abs(self.__coefficients[i]))
+            if current_degree != 0:
+                if current_degree != 1:
+                    string += 'x^' + str(current_degree)
+                else:
+                    string += 'x'
+            current_degree -= 1
+            i += 1
+            while current_degree >= 0:
+                if self.__coefficients[i] != 0:
+                    if self.__coefficients[i] < 0:
+                        string += ' - '
+                    else:
+                        string += ' + '
+                    if abs(self.__coefficients[i]) != 1 or current_degree == 0:
+                        string += str(abs(self.__coefficients[i]))
+                    if current_degree != 0:
+                        if current_degree != 1:
+                            string += 'x^' + str(current_degree)
+                        else:
+                            string += 'x'
+                current_degree -= 1
+                i += 1
+        return string
+
+    def __repr__(self):
+        return 'Polynomial({})'.format(str(self.__coefficients))
+
+    def __call__(self, x):
+        if isinstance(x, (int, float)):
+            res = 0
+            for i, a in enumerate(self.__coefficients):
+                res += a * x ** (self.degree() - i)
+            return res
+        else:
+            raise TypeError('Invalid argument in call. Need list or tuple.')
+
+    def __iter__(self):
+        for a in self.__coefficients:
+            yield a
+
+    def __getitem__(self, degree):
+        if isinstance(degree, int):
+            if degree >= 0:
+                index = self.degree() - degree
+                if index >= 0:
+                    return self.__coefficients[index]
+                else:
+                    return 0
+            else:
+                raise ValueError('Invalid argument in getitem. Need int >= 0.')
+        else:
+            raise TypeError('Invalid argument in getitem. Need int >= 0.')
+
+    def __setitem__(self, degree, value):
+        if isinstance(degree, int) and isinstance(value, int):
+            if degree >= 0:
+                if degree <= self.degree():
+                    index = self.degree() - degree
+                    del self.__coefficients[index]
+                    self.__coefficients.insert(index, value)
+                else:
+                    differences = degree - self.degree() - 1
+                    zeros = [0]*differences
+                    self.__coefficients = zeros + self.__coefficients
+                    self.__coefficients.insert(0, value)
+                self.__remove_zeros_polynomial()
+            else:
+                raise ValueError('Invalid argument in setitem. For degree need: int >= 0.')
+        else:
+            raise TypeError('Invalid argument in setitem. For degree need: int >= 0. For value need: int.')
+
+    def __eq__(self, other):
+        if isinstance(other, Polynomial):
+            return self.get_coefficients() == other.get_coefficients()
+        else:
+            raise TypeError('Invalid argument in ==. Need Polynomial.')
+
+    def __neg__(self):
+        return Polynomial([-a for a in self.__coefficients])
+
+    def __pos__(self):
+        return self
+
+    @staticmethod
+    def zip_two_polynomial(poly1, poly2):
+        longest_polynomial = ''
+        max_len = 0
+        min_len = 0
+        if len(poly1.get_coefficients()) >= len(poly2.get_coefficients()):
+            max_len = len(poly1.get_coefficients())
+            min_len = len(poly2.get_coefficients())
+            longest_polynomial = '1'
+        else:
+            max_len = len(poly2.get_coefficients())
+            min_len = len(poly1.get_coefficients())
+            longest_polynomial = '2'
+        differences = max_len - min_len
+
+        j = min_len - 1
+        for i in range(max_len-1, -1, -1):
+            if i >= differences and longest_polynomial == '1' and max_len != min_len:
+                yield (poly1[i], 0)
+            elif i >= differences and longest_polynomial == '2' and max_len != min_len:
+                yield (poly2[i], 0)
+            elif longest_polynomial == '1':
+                yield (poly1[i], poly2[j])
+                j -= 1
+            else:
+                yield (poly2[i], poly1[j])
+                j -= 1
+
+    def __add__(self, other):
+        if isinstance(other, Polynomial):
+            res = [a + b for a, b in Polynomial.zip_two_polynomial(self, other)]
+            return Polynomial(res)
+        elif isinstance(other, int):
+            res = self.get_coefficients()
+            res[self.degree()] += other
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in add. Need Polynomial or int.')
+
+    def __radd__(self, other):
+        if isinstance(other, int):
+            res = self.get_coefficients()
+            res[self.degree()] += other
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in radd. Need int.')
+
+    def __sub__(self, other):
+        if isinstance(other, Polynomial):
+            res = [a - b for a, b in Polynomial.zip_two_polynomial(self, other)]
+            return Polynomial(res)
+        elif isinstance(other, int):
+            res = self.get_coefficients()
+            res[self.degree()] -= other
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in sub. Need Polynomial or int.')
+
+    def __rsub__(self, other):
+        if isinstance(other, int):
+            res = self.get_coefficients()
+            res[self.degree()] -= other
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in rsub. Need int.')
+
+    def __mul__(self, other):
+        if isinstance(other, Polynomial):
+            res_degree = self.degree() + other.degree()
+            res = [0] * (res_degree + 1)
+            for index_a, a in enumerate(self.get_coefficients()):
+                for index_b, b in enumerate(other.get_coefficients()):
+                    degree_a = self.degree() - index_a
+                    degree_b = other.degree() - index_b
+                    index_res = res_degree - (degree_a + degree_b)
+                    res[index_res] = res[index_res] + a * b
+            return Polynomial(res)
+        elif isinstance(other, int):
+            res = [other * a for a in self.get_coefficients()]
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in mul. Need Polynomial or int.')
+
+    def __rmul__(self, other):
+        if isinstance(other, int):
+            res = [other * a for a in self.get_coefficients()]
+            return Polynomial(res)
+        else:
+            raise TypeError('Invalid argument in rmul. Need Polynomial or int.')
---
+++ polynomial_tests.py
@@ -0,0 +1,491 @@
+import unittest
+from polynomial import Polynomial
+
+
+class TestPolynomialInit(unittest.TestCase):
+    def test_empty(self):
+        correct_answer = (0, [0])
+        p = Polynomial()
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_with_list(self):
+        list_coefficients = [1, 2, 3]
+        correct_answer = (2, list_coefficients)
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_with_garbage_list(self):
+        list_coefficients = [1, 'abc', 3.4]
+        self.assertRaises(ValueError, Polynomial, list_coefficients)
+
+    def test_with_tuple(self):
+        tuple_coefficients = (1, 2, 3)
+        correct_answer = (2, list(tuple_coefficients))
+        p = Polynomial(tuple_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_with_garbage_tuple(self):
+        tuple_coefficients = (1, 'abc', 3.4)
+        self.assertRaises(ValueError, Polynomial, tuple_coefficients)
+
+    def test_empty_typle(self):
+        tuple_coefficients = ()
+        correct_answer = (0, [0])
+        p = Polynomial(tuple_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_empty_list(self):
+        list_coefficients = []
+        correct_answer = (0, [0])
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_constant(self):
+        list_coefficients = [1, ]
+        correct_answer = (0, list_coefficients)
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_second_degree(self):
+        list_coefficients = [3, 2, 1]
+        correct_answer = (2, list_coefficients)
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_list_with_zeros(self):
+        list_coefficients = [0, 0, 0]
+        correct_answer = (0, [0])
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_list_with_zeros_in_head(self):
+        list_coefficients = [0, 0, 0, 1, 0, 0, 1]
+        correct_answer = (3, [1, 0, 0, 1])
+        p = Polynomial(list_coefficients)
+        answer = (p.degree(), p.get_coefficients())
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument_string(self):
+        self.assertRaises(TypeError, Polynomial, 'abc')
+
+    def test_invalid_argument_float(self):
+        self.assertRaises(TypeError, Polynomial, 1.1)
+
+    def test_polynomial_equal_coefficients(self):
+        list_coefficients = [1, 2, 3]
+        p1 = Polynomial(list_coefficients)
+        p2 = Polynomial(p1)
+        self.assertEqual(p1.get_coefficients(), p2.get_coefficients())
+
+    def test_polynomial_not_equal_lists_coefficients(self):
+        list_coefficients = [3, 2, 1]
+        p1 = Polynomial(list_coefficients)
+        p2 = Polynomial(p1)
+        self.assertIsNot(p1.get_coefficients(), p2.get_coefficients())
+
+
+class TestPolynomialStr(unittest.TestCase):
+    def test_positive_full_polynomial_degree_3(self):
+        correct_answer = '4x^3 + 3x^2 + 2x + 1'
+        p = Polynomial([4, 3, 2, 1])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+    def test_negative_full_polynomial_degree_3(self):
+        correct_answer = '- 4x^3 - 3x^2 - 2x - 1'
+        p = Polynomial([-4, -3, -2, -1])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+    def test_mixed_full_polynomial_degree_3(self):
+        correct_answer = '- 4x^3 + 3x^2 + x - 1'
+        p = Polynomial([-4, 3, 1, -1])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+    def test_mixed_not_full_polynomial_degree_3(self):
+        correct_answer = '- x^3 - 2x + 1'
+        p = Polynomial([-1, 0, -2, 1])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+    def test_negative_full_polynomial_degree_0(self):
+        correct_answer = '- 10'
+        p = Polynomial([-10])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+    def test_mixed_not_full_polynomial_degree_1(self):
+        correct_answer = '- 10x'
+        p = Polynomial([-10, 0])
+        answer = str(p)
+        self.assertEqual(correct_answer, answer)
+
+
+class TestPolynomialRepr(unittest.TestCase):
+    def test_positive_full_polynomial_degree_3(self):
+        correct_answer = 'Polynomial([4, 3, 2, 1])'
+        p = Polynomial([4, 3, 2, 1])
+        answer = repr(p)
+        self.assertEqual(correct_answer, answer)
+
+
+class TestPolynomialCall(unittest.TestCase):
+    def test_call_int_polynomial_degree_3(self):
+        correct_answer = 4
+        p = Polynomial([1, 1, 1, 1])
+        answer = p(1)
+        self.assertEqual(correct_answer, answer)
+
+    def test_call_int_polynomial_degree_0(self):
+        correct_answer = 10
+        p = Polynomial([10])
+        answer = p(1)
+        self.assertEqual(correct_answer, answer)
+
+    def test_call_float_polynomial_degree_1(self):
+        correct_answer = 6.0
+        p = Polynomial([2, 1])
+        answer = p(2.5)
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p, 'abc')
+
+
+class TestPolynomialIter(unittest.TestCase):
+    def test_full_polynomial_degree_3(self):
+        correct_answer = '4 3 2 1 '
+        p = Polynomial([4, 3, 2, 1])
+        answer = ''
+        for a in p:
+            answer += '{} '.format(a)
+        self.assertEqual(correct_answer, answer)
+
+    def test_not_full_polynomial_degree_3(self):
+        correct_answer = '4 0 0 1 '
+        p = Polynomial([4, 0, 0, 1])
+        answer = ''
+        for a in p:
+            answer += '{} '.format(a)
+        self.assertEqual(correct_answer, answer)
+
+
+class TestPolynomialGetitem(unittest.TestCase):
+    def test_get_existing_element(self):
+        correct_answer = 3
+        p = Polynomial([4, 3, 2, 1])
+        answer = p[2]
+        self.assertEqual(correct_answer, answer)
+
+    def test_get_not_existing_element(self):
+        correct_answer = 0
+        p = Polynomial([4, 3, 2, 1])
+        answer = p[10]
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument_less_0(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(ValueError, p.__getitem__, -1)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__getitem__, 'abc')
+
+
+class TestPolynomialSetitem(unittest.TestCase):
+    def test_set_element_less_degree(self):
+        correct_p = Polynomial([4, 2, 1])
+        p = Polynomial([3, 2, 1])
+        p[2] = 4
+        self.assertEqual(correct_p, p)
+
+    def test_set_element_more_degree(self):
+        correct_p = Polynomial([4, 3, 2, 1])
+        p = Polynomial([3, 2, 1])
+        p[3] = 4
+        self.assertEqual(correct_p, p)
+
+    def test_set_element_more_more_degree(self):
+        correct_p = Polynomial([6, 0, 0, 3, 2, 1])
+        p = Polynomial([3, 2, 1])
+        p[5] = 6
+        self.assertEqual(correct_p, p)
+
+    def test_set_zero_element_in_max_degree(self):
+        correct_p = Polynomial([3, 2, 1])
+        p = Polynomial([6, 0, 0, 3, 2, 1])
+        p[5] = 0
+        self.assertEqual(correct_p, p)
+
+    def test_invalid_argument_less_0(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(ValueError, p.__setitem__, -1, 4)
+
+    def test_invalid_argument_degree_not_int(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__setitem__, 0.1, 4)
+
+    def test_invalid_argument_value_not_int(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__setitem__, 3, 0.1)
+
+
+class TestPolynomialEq(unittest.TestCase):
+    def test_equal_polynomial(self):
+        list_coefficients = [4, 3, 2, 1]
+        p1 = Polynomial(list_coefficients)
+        p2 = Polynomial(list_coefficients)
+        self.assertTrue(p1 == p2)
+
+    def test_not_equal_polynomial(self):
+        p1 = Polynomial([1, 2, 3, 4])
+        p2 = Polynomial([4, 3, 2, 1])
+        self.assertFalse(p1 == p2)
+
+    def test_not_equal_polynomial_with_different_length(self):
+        p1 = Polynomial([1, 2, 3, 4])
+        p2 = Polynomial([3, 2, 1])
+        self.assertFalse(p1 == p2)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__eq__, 'abc')
+
+
+class TestPolynomialNe(unittest.TestCase):
+    def test_equal_polynomial(self):
+        list_coefficients = [4, 3, 2, 1]
+        p1 = Polynomial(list_coefficients)
+        p2 = Polynomial(list_coefficients)
+        self.assertFalse(p1 != p2)
+
+    def test_not_equal_polynomial(self):
+        p1 = Polynomial([1, 2, 3, 4])
+        p2 = Polynomial([4, 3, 2, 1])
+        self.assertTrue(p1 != p2)
+
+    def test_not_equal_polynomial_with_different_length(self):
+        p1 = Polynomial([1, 2, 3, 4])
+        p2 = Polynomial([3, 2, 1])
+        self.assertTrue(p2 != p1)
+
+
+class TestPolynomialNeg(unittest.TestCase):
+    def test_created_new_polynomial(self):
+        p1 = Polynomial([4, -3, -2, 1])
+        p2 = -p1
+        self.assertIsNot(p1.get_coefficients(), p2.get_coefficients())
+
+    def test_coefficients_changed_correctly(self):
+        p1 = Polynomial([4, -3, -2, 1])
+        p2 = -p1
+        self.assertEqual([-4, 3, 2, -1], p2.get_coefficients())
+
+
+class TestPolynomialPos(unittest.TestCase):
+    def test_not_created_new_polynomial(self):
+        p1 = Polynomial([4, -3, -2, 1])
+        p2 = +p1
+        self.assertIs(p1.get_coefficients(), p2.get_coefficients())
+
+    def test_coefficients_changed_correctly(self):
+        p1 = Polynomial([4, -3, -2, 1])
+        p2 = +p1
+        self.assertEqual([4, -3, -2, 1], p2.get_coefficients())
+
+
+class TestPolynomialZipTwoPolynomial(unittest.TestCase):
+    def test_polynomial_equal_length(self):
+        p1 = Polynomial([-1, 2, -3, 4])
+        p2 = Polynomial([4, -3, 2, -1])
+        correct_answer = '(-1, 4) (2, -3) (-3, 2) (4, -1) '
+        answer = ''
+        for elem in Polynomial.zip_two_polynomial(p1, p2):
+            answer += '{} '.format(elem)
+        self.assertEqual(correct_answer, answer)
+
+    def test_polynomial_not_equal_length_1(self):
+        p1 = Polynomial([1, 2, 3, 4])
+        p2 = Polynomial([2, 1])
+        correct_answer = '(1, 0) (2, 0) (3, 2) (4, 1) '
+        answer = ''
+        for elem in Polynomial.zip_two_polynomial(p1, p2):
+            answer += '{} '.format(elem)
+        self.assertEqual(correct_answer, answer)
+
+    def test_polynomial_not_equal_length_2(self):
+        p1 = Polynomial([2, 1])
+        p2 = Polynomial([1, 2, 3, 4])
+        correct_answer = '(1, 0) (2, 0) (3, 2) (4, 1) '
+        answer = ''
+        for elem in Polynomial.zip_two_polynomial(p1, p2):
+            answer += '{} '.format(elem)
+        self.assertEqual(correct_answer, answer)
+
+
+class TestPolynomialAdd(unittest.TestCase):
+    def test_equal_polynomial(self):
+        correct_answer = Polynomial([2, 2, 2, 2])
+        p1 = Polynomial([1, 1, 1, 1])
+        p2 = Polynomial([1, 1, 1, 1])
+        answer = p1 + p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_not_equal_polynomial(self):
+        correct_answer = Polynomial([1, 1, 2, 2])
+        p1 = Polynomial([1, 1, 1, 1])
+        p2 = Polynomial([1, 1])
+        answer = p1 + p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_sum_equal_zero(self):
+        correct_answer = Polynomial([0])
+        p1 = Polynomial([-1, -1])
+        p2 = Polynomial([1, 1])
+        answer = p1 + p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_const(self):
+        correct_answer = Polynomial([1, 2])
+        p1 = Polynomial([1, 1])
+        answer = p1 + 1
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__add__, 'abc')
+
+
+class TestPolynomialRadd(unittest.TestCase):
+    def test_const(self):
+        correct_answer = Polynomial([1, 2])
+        p1 = Polynomial([1, 1])
+        answer = 1 + p1
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__radd__, 'abc')
+
+
+class TestPolynomialSub(unittest.TestCase):
+    def test_equal_polynomial(self):
+        correct_answer = Polynomial([2, -2, -2, 2])
+        p1 = Polynomial([3, -3, -3, 3])
+        p2 = Polynomial([1, -1, -1, 1])
+        answer = p1 - p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_not_equal_polynomial(self):
+        correct_answer = Polynomial([1, 1, 1, 0])
+        p1 = Polynomial([1, 1, 2, 1])
+        p2 = Polynomial([1, 1])
+        answer = p1 - p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_sub_equal_zero(self):
+        correct_answer = Polynomial([0])
+        p1 = Polynomial([1, 1])
+        p2 = Polynomial([1, 1])
+        answer = p1 - p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_const(self):
+        correct_answer = Polynomial([1, 0])
+        p1 = Polynomial([1, 1])
+        answer = p1 - 1
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__sub__, 2.3)
+
+
+class TestPolynomialRsub(unittest.TestCase):
+    def test_const(self):
+        correct_answer = Polynomial([1, 0])
+        p1 = Polynomial([1, 1])
+        answer = 1 - p1
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__rsub__, {})
+
+
+class TestPolynomialMul(unittest.TestCase):
+    def test_equal_polynomial(self):
+        correct_answer = Polynomial([1, 2, 1])
+        p1 = Polynomial([1, 1])
+        p2 = Polynomial([1, 1])
+        answer = p1 * p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_polynomial(self):
+        correct_answer = Polynomial([-1, 6, -11, 12])
+        p1 = Polynomial([1, -2, 3])
+        p2 = Polynomial([-1, 4])
+        answer = p1 * p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_zeroing_polynomial(self):
+        correct_answer = Polynomial([1, 0, 2, 0, 0, 0])
+        p1 = Polynomial([1, 0, 0, 0])
+        p2 = Polynomial([1, 0, 2])
+        answer = p1 * p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_empty_polynomial(self):
+        correct_answer = Polynomial([0])
+        p1 = Polynomial()
+        p2 = Polynomial([-1, 6, -11, 12])
+        answer = p1 * p2
+        self.assertEqual(correct_answer, answer)
+
+    def test_const_zero(self):
+        correct_answer = Polynomial([0])
+        p1 = Polynomial([1, 1])
+        answer = p1 * 0
+        self.assertEqual(correct_answer, answer)
+
+    def test_const(self):
+        correct_answer = Polynomial([3, 0, 6])
+        p1 = Polynomial([1, 0, 2])
+        answer = p1 * 3
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__mul__, 2.3)
+
+
+class TestPolynomialRmul(unittest.TestCase):
+    def test_const_zero(self):
+        correct_answer = Polynomial([0])
+        p1 = Polynomial([1, 1])
+        answer = 0 * p1
+        self.assertEqual(correct_answer, answer)
+
+    def test_const(self):
+        correct_answer = Polynomial([3, 0, 6])
+        p1 = Polynomial([1, 0, 2])
+        answer = 3 * p1
+        self.assertEqual(correct_answer, answer)
+
+    def test_invalid_argument(self):
+        p = Polynomial([1, 2, 3])
+        self.assertRaises(TypeError, p.__rmul__, 'abc')
+
+
+if __name__ == '__main__':
+    unittest.main()
